5de6603aaa69ce590ee93e5a98185836708b71a4
felynda99 <49071447+felynda99@users.noreply.github.com>
2019-03-29 22:14:21 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MenuUtama from './src/QuizC1715051102/MenuUtama';
+import CuacaA from './src/04Api/CuacaA';
+import CuacaB from './src/04Api/CuacaB';
+
+export default class App extends React.Component {
+	render() {
+		return (
+		<CuacaB />
+		);
+	}
+}
